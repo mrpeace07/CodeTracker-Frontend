40255d93fa5375210a98d98478bb6ddc2e344ac1
Chinmay-Ankolekar <chinmaysankolekar@gmail.com>
2024-07-14 14:10:11 +0530

    chore: Update landing page link to GitHub repository

--- leetcode/src/components/LandingPage.jsx
+++ leetcode/src/components/LandingPage.jsx
@@ -21,6 +21,7 @@ const LandingPage = () => {
           <div className="flex justify-center space-x-4">
             <a
               href="https://github.com/Chinmay-Ankolekar/Leetcode-Tracker-Extension"
+              target="_blank"
               className="bg-blue-600 hover:bg-blue-700 text-white py-3 px-6 rounded-lg text-lg font-medium transition duration-300"
             >
               Download Extension
